c776aeaa8c4c9b29ad933d27a54cbbff143aa2ab
wgtmac <ustcwg@gmail.com>
2015-07-30 12:53:30 -0400

    Initial Commit

---
+++ TakeoutAssistant/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TakeoutAssistant
+//
+//  Created by Gang Wu on 7/30/15.
+//  Copyright (c) 2015 Team3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TakeoutAssistant/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TakeoutAssistant
+//
+//  Created by Gang Wu on 7/30/15.
+//  Copyright (c) 2015 Team3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
