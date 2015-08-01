28844d1500d059a430dae5c9f549ebe43689b45a
hxzjpp <zhuangjing@gmail.com>
2015-08-01 14:20:30 -0400

    add new detail controller

---
+++ TakeoutAssistant/RetaurantDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  RetaurantDetailViewController.h
+//  TakeoutAssistant
+//
+//  Created by Z J on 8/1/15.
+//  Copyright (c) 2015 Team3. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RetaurantDetailViewController : NSObject
+
+@end
---
+++ TakeoutAssistant/RetaurantDetailViewController.m
@@ -0,0 +1,13 @@
+//
+//  RetaurantDetailViewController.m
+//  TakeoutAssistant
+//
+//  Created by Z J on 8/1/15.
+//  Copyright (c) 2015 Team3. All rights reserved.
+//
+
+#import "RetaurantDetailViewController.h"
+
+@implementation RetaurantDetailViewController
+
+@end
